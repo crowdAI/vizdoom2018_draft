66edb445c6687a7f4582631acd05a75931209f6b
S.P. Mohanty <spmohanty91@gmail.com>
2018-04-19 14:43:44 +0200

    Add doc on viewing logs

--- debug.sh
+++ debug.sh
@@ -13,3 +13,8 @@ docker tag cig2017_random localhost:5000/cig2017_random
 docker tag cig2017_host localhost:5000/cig2017_host
 docker push localhost:5000/cig2017_random
 docker push localhost:5000/cig2017_host
+
+# Then view logs of the host by :
+# kubectl logs vizdoom2018-grading-pod-debug -c host -f
+# and of submission-01 container by :
+# kubectl logs vizdoom2018-grading-pod-debug -c submission-01 -f
